6b2835baa0f8775fa08a0f6ba6dd2bab89a2db72
Joseph Empedrado <joseph.empedrado.vc@gmail.com>
2015-04-17 17:19:49 +0800

    List references edit and delete data

--- _application/controllers/Form.php
+++ _application/controllers/Form.php
@@ -297,10 +297,10 @@ class Form extends MY_Controller {
         if(is_posted()===false){
             redirect(base_url().'index.php/admin', 'refresh');
         }
-        chmod('upload/', 0777);
+        //chmod('upload', 0777);
         $temp_dir = 'upload/'.$this->input->post('_tmp_');
         if (!is_dir($temp_dir)) {
-            if(!mkdir($temp_dir, 0777, TRUE)){
+            if(!mkdir($temp_dir, 0755)){
                 die('Failed to create folder.');
             }
 
