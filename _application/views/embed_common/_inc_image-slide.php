f1cee1c55a79bce1ec364f9811664392a14aab47
Joseph Empedrado <joseph.empedrado.vc@gmail.com>
2015-05-17 15:32:08 +0800

    Added image slider on stock images

---
+++ _application/views/embed_common/_inc_image-slide.php
@@ -0,0 +1,47 @@
+<div id="image-slide-carousel-top" class="carousel slide no-margin" data-ride="carousel">
+    <!-- Indicators -->
+    <ol id="image-slide-carousel-indicators" class="carousel-indicators">
+        <li data-target="#image-slide-carousel-top" data-slide-to="0" class="active"></li>
+        <li data-target="#image-slide-carousel-top" data-slide-to="1"></li>
+        <li data-target="#image-slide-carousel-top" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div id="image-slide-carousel-inner" class="carousel-inner">
+
+        <div class="item active">
+            <img  style="width:100%;" id="img_slide1" src="<?php echo base_url().'assets/images/slides/fuso-truck-1.jpg'; ?>" alt="house 1">
+            <div class="carousel-caption">
+                <h2 class="font-normal text-uppercase font-white-black">Class A Quality</h2>
+                <p class="text-shadow">Certified Used can save on cost but get the same quality
+                </p>
+            </div>
+        </div>
+
+        <div class="item">
+            <img style="width:100%;" src="<?php echo base_url().'assets/images/slides/heavy-equipment-1.jpg'; ?>" alt="house 2">
+            <div class="carousel-caption">
+                <h2 class="font-normal text-uppercase font-white-black">Heavy Equipments</h2>
+                <p class="text-shadow">We have the latest models of reconditioned Japan surplus heavy equipments and trucks.</p>
+            </div>
+        </div>
+
+        <div class="item">
+            <img  style="width:100%;" src="<?php echo base_url().'assets/images/slides/hino-truck-1.jpg'; ?>" alt="Trucks">
+            <div class="carousel-caption">
+                <h2  class="font-normal text-uppercase font-white-black">Trucks</h2>
+                <p class="text-shadow">Selected Japan used heavy equipments and trucks.</p>
+            </div>
+        </div>
+
+
+    </div>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#image-slide-carousel-top" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+    </a>
+    <a class="right carousel-control" href="#image-slide-carousel-top" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+    </a>
+</div>
